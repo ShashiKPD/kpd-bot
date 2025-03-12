e6e707d85962d017f7461d3e1c8776ca32bfdd06
Shashikant Prasad <shashikantprasad107@gmail.com>
2025-03-12 19:03:12 +0530

    added keep alive endpoint for render.com

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { initializeWhatsappClient } from "./whatsappClient/whatsappClient.js";
-import { connectDB } from "./db/index.js"
+import { connectDB } from "./db/index.js";
 import { Group } from "./models/group.model.js";
 import { Task } from "./models/task.model.js";
 
@@ -10,5 +10,17 @@ import LocalStore from "./db/local.store.js";
 export const groupStore = new LocalStore(600000, Group);
 export const taskStore = new LocalStore(600000, Task); // 10 minutes
 
-
 connectDB();
+
+import express from "express";
+
+const app = express();
+const port = 3500;
+
+app.get("/keep-alive", (req, res) => {
+  res.send("The Bot is alive. 🤖");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
